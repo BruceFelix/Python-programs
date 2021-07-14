8c10453ed81ef1dca2c10a66a51df88614fc897a
BruceFelix <brucefelixm@gmail.com>
2021-07-14 06:11:45 +0300

    starting day 4 work

---
+++ 30days of python/Day4/day4.py
@@ -0,0 +1,10 @@
+# Concatenate
+# 1.
+concatinating = ['Thirty', 'Days', 'Of', 'Python']
+result = " ".join(concatinating)
+print(result)
+
+#2.
+joining = ['Coding', 'For', 'All']
+res = " ".join(joining)
+print(res)
