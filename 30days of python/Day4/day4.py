411685ba3f48db37314236c3313aad54ef3992e3
BruceFelix <brucefelixm@gmail.com>
2021-07-16 08:46:10 +0300

    more progress in day 4

--- 30days of python/Day4/day4.py
+++ 30days of python/Day4/day4.py
@@ -8,3 +8,17 @@ print(result)
 joining = ['Coding', 'For', 'All']
 res = " ".join(joining)
 print(res)
+
+#3 
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[7:14])
+print(company.find("Coding"))
+print(company.replace('Coding', 'Python'))
+
