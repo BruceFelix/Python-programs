be54d2768ccc65e86c05de8ba6b57188b9c632d2
BruceFelix <brucefelixm@gmail.com>
2021-07-20 09:49:38 +0300

    Day 5 began its all about lists and things you can do to list

---
+++ 30days of python/Day5/day5.py
@@ -0,0 +1,11 @@
+#1 
+empty_list = []
+
+#2
+shoes_list = ['Jordan','Timberland', "clarks", 'Gucci','Loafers']
+
+#3
+print(len(shoes_list))
+
+#4
+print(shoes_list[0],shoes_list[2], shoes_list[-1])
